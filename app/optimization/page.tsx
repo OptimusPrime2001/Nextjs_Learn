6883e03128e7ae2d43cebf2e8ab963aec69c593a
letrunks <trungbts3@gmail.com>
2025-03-21 00:02:39 +0700

    refactor: update dependencies and refactor image component
    - Updated package.json to include @tailwindcss/cli and @tailwindcss/postcss
    - Refactored BlurImage component to simplify props and improve styling
    - Adjusted postcss.config.js to use @tailwindcss/postcss plugin
    - Modified .gitignore to include .env* files

--- app/optimization/page.tsx
+++ app/optimization/page.tsx
@@ -9,14 +9,14 @@ export async function getImages() {
   return clubs;
 }
 export default async function Page() {
-  const images = await getImages();
+  const clubs = await getImages();
   return (
     <div className='px-4 py-16 mx-auto max-w-2xl sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
       <h1 className='m-4 text-2xl text-center'>Page</h1>
 
-      <div className='grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
-        {images &&
-          images.map((image) => <BlurImage key={image.id} image={image} />)}
+      <div className='grid grid-cols-1 gap-x-6 gap-y-10 bg-blue-200 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
+        {clubs &&
+          clubs.map((club) => <BlurImage key={club.id} href={club.logo_img} />)}
       </div>
     </div>
   );
--- src/components/atoms/BlurImage.tsx
+++ src/components/atoms/BlurImage.tsx
@@ -3,32 +3,29 @@ import { classNames } from '@/src/utils/common';
 import Image from 'next/image';
 import { useState } from 'react';
 
-type Image = {
-  id: string;
-  name: string;
+type ImageProps = {
+  id?: string;
+  name?: string;
   href: string;
-  username: string;
-  imageSrc: string;
+  username?: string;
+  imageSrc?: string;
 };
-export default function BlurImage({ image }: { image: Image }) {
+export default function BlurImage(props: ImageProps) {
   const [isLoading, setLoading] = useState(true);
 
   return (
-    <div className='aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg group bg-gray-200 xl:aspect-w-5 xl:aspect-h-5'>
+    <div className='relative w-10 h-10 rounded-lg group'>
       <Image
         alt=''
-        src={image.href}
-        style={{
-          width: '100%',
-          objectFit: 'cover',
-        }}
-        width={500}
-        height={500}
+        src={props.href }
+        fill
+        objectFit='contain'
+        priority
         className={classNames(
-          'duration-700 w-full object-cover aspect-square h-auto ease-in-out group-hover:opacity-75',
+          'w-full h-auto bg-blue-500 duration-1000 ease-in-out aspect-square group-hover:opacity-75',
           isLoading
-            ? 'scale-110 blur-2xl grayscale'
-            : 'scale-100 blur-0 grayscale-0',
+            ? 'blur-2xl grayscale scale-110'
+            : 'grayscale-0 scale-100 blur-0',
         )}
         onLoadingComplete={() => setLoading(false)}
       />
