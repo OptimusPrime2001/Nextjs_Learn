c17ad6773e327e480bc95fee33ee2bb094b27069
letrunks <trungbts3@gmail.com>
2025-03-27 23:55:45 +0700

    feat: enable experimental HTTPS in dev mode and add HTTPS agent

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 import Image from 'next/image';
 import Counter from './counter';
 import imgUrl from '@/public/bg-1.jpg';
+import { Agent } from 'https';
 
 export default async function Home() {
+  const httpsAgent = new Agent({
+    rejectUnauthorized: false,
+  });
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL as string,{
+    agent:process.env.NODE_ENV === 'development' ? httpsAgent:undefined,
+  });
+  if (!response.ok) {
+    throw new Error('Failed to fetch from NestJS');
+  }
+  console.log(response);
   return (
     <main className='flex flex-col justify-between items-center p-24 min-h-screen text-4xl'>
       Hello nextJs | Home Page
