2ddb58bd5878cddbdef63dfe88fbf31d1a0dc03f
letrunks <trungbts3@gmail.com>
2025-03-29 21:06:42 +0700

    feat: add HTTPS agent and enhance fetch request in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,31 @@
 import Image from 'next/image';
 import Counter from './counter';
 import imgUrl from '@/public/bg-1.jpg';
+import { Agent } from 'https';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export default async function Home() {
-  // const httpsAgent = new Agent({
-  //   rejectUnauthorized: false,
-  // });
+  const httpsAgent = new Agent({
+    rejectUnauthorized: false,
+  });
   const response = await fetch(`${API_URL}/hello-word`,{
     method: 'GET',
+    // @ts-ignore - Ignoring type error for custom agent property
+    agent: httpsAgent,
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json',
+    },
   }).then((res) => {
     return res.text(); 
   });
   if (!response) {
     throw new Error('Failed to fetch from NestJS');
   } 
-  console.log(response);
   return (
     <main className='flex flex-col justify-between items-center p-24 min-h-screen text-4xl'>
       Hello nextJs | Home Page
+      <b>{response}</b>
       <Counter />
       <Image src={imgUrl} alt='test' />
     </main>
